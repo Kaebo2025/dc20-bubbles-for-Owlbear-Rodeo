f9646df3b6de174dd50ea5d3af118bcf1c20d714
Kaebo2025 <ccabano19@gmail.com>
2026-03-02 01:00:37 -0600

    DC20: metadata keys (hp/max hp/pd/ad

--- src/metadataHelpers/itemMetadataIds.ts
+++ src/metadataHelpers/itemMetadataIds.ts
@@ -1,14 +1,9 @@
-export type StatMetadataID =
-  | "health"
-  | "max health"
-  | "temporary health"
-  | "armor class"
-  | "hide";
+export type StatMetadataID = "hp" | "max hp" | "pd" | "ad" | "hide";
 
-export const HEALTH_METADATA_ID: StatMetadataID = "health";
-export const MAX_HEALTH_METADATA_ID: StatMetadataID = "max health";
-export const TEMP_HEALTH_METADATA_ID: StatMetadataID = "temporary health";
-export const ARMOR_CLASS_METADATA_ID: StatMetadataID = "armor class";
+export const HP_METADATA_ID: StatMetadataID = "hp";
+export const MAX_HP_METADATA_ID: StatMetadataID = "max hp";
+export const PD_METADATA_ID: StatMetadataID = "pd";
+export const AD_METADATA_ID: StatMetadataID = "ad";
 export const HIDE_METADATA_ID: StatMetadataID = "hide";
 
 export type TokenSortingMetadataID = "group" | "index";
