a9e1965643c6a354f47bf7b35a160f0eedb72b4a
Kaebo2025 <ccabano19@gmail.com>
2026-03-02 01:06:17 -0600

    DC20: metadata keys (hp/max hp/temp hp/pd/ad

--- src/metadataHelpers/itemMetadataIds.ts
+++ src/metadataHelpers/itemMetadataIds.ts
@@ -1,7 +1,8 @@
-export type StatMetadataID = "hp" | "max hp" | "pd" | "ad" | "hide";
+export type StatMetadataID = "hp" | "max hp" | "temp hp" | "pd" | "ad" | "hide";
 
 export const HP_METADATA_ID: StatMetadataID = "hp";
 export const MAX_HP_METADATA_ID: StatMetadataID = "max hp";
+export const TEMP_HP_METADATA_ID: StatMetadataID = "temp hp";
 export const PD_METADATA_ID: StatMetadataID = "pd";
 export const AD_METADATA_ID: StatMetadataID = "ad";
 export const HIDE_METADATA_ID: StatMetadataID = "hide";
