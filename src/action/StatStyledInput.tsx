a68ec8ad14494f4e4d41a9b4b077eb688a90b02f
Kaebo2025 <ccabano19@gmail.com>
2026-03-02 02:10:47 -0600

    DC20: update StatStyledInput for hp/tempHp/pd/ad colors + labels

--- src/action/StatStyledInput.tsx
+++ src/action/StatStyledInput.tsx
@@ -25,11 +25,18 @@ export default function StatStyledInput({
             "h-[32px]",
             "w-[60px]",
             {
+              // HP / Max HP
               "bg-stat-red/10 dark:bg-stat-red-dark/5":
-                name === "health" || name === "maxHealth",
-              "bg-stat-green/10 dark:bg-stat-green-dark/5":
-                name === "tempHealth",
-              "bg-stat-blue/10 dark:bg-stat-blue-dark/5": name === "armorClass",
+                name === "hp" || name === "maxHp",
+
+              // Temp HP
+              "bg-stat-yellow/10 dark:bg-stat-yellow-dark/5": name === "tempHp",
+
+              // PD
+              "bg-stat-blue/10 dark:bg-stat-blue-dark/5": name === "pd",
+
+              // AD
+              "bg-stat-green/10 dark:bg-stat-green-dark/5": name === "ad",
             },
             inputProps?.className,
           )}
@@ -44,13 +51,17 @@ export default function StatStyledInput({
 
 const nameToLabel = (name: InputName) => {
   switch (name) {
-    case "health":
-      return "Current Hit Points";
-    case "maxHealth":
-      return "Hit Points Maximum";
-    case "tempHealth":
-      return "Temporary Hit Points";
-    case "armorClass":
-      return "Armor Class";
+    case "hp":
+      return "HP";
+    case "maxHp":
+      return "Max HP";
+    case "tempHp":
+      return "Temp HP";
+    case "pd":
+      return "PD";
+    case "ad":
+      return "AD";
+    case "hideStats":
+      return "Hide Stats";
   }
 };
